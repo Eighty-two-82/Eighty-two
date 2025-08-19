acd0e56d0c8d1a921d92778bcae2fef96a42f317
yutongh8 <yutongh8@student.unimelb.edu.au>
2025-08-19 21:59:28 +1000

    Update UserService.java

--- backend/src/main/java/com/springboot/springbootlogindemo/service/UserService.java
+++ backend/src/main/java/com/springboot/springbootlogindemo/service/UserService.java
@@ -4,7 +4,6 @@ import com.springboot.springbootlogindemo.domain.User;
 
 public interface UserService {
     /**
-     * 登录业务逻辑
      * @param uname Account name
      * @param password Password
      * @return
@@ -12,7 +11,6 @@ public interface UserService {
     User loginService(String uname, String password);
 
     /**
-     * 注册业务逻辑
      * @param user User object to register, the primary key uid in properties should be empty, if uid is not empty it may overwrite existing user
      * @return
      */
