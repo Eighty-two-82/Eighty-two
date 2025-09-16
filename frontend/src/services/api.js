09e28d7d28eb58a1eeaa8abb041c4d1fafafd165
“yuanmengy” <yuanmengy@student.unimelb.edu.au>
2025-09-16 22:43:20 +1000

    Build frontend for deploy

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -4,8 +4,8 @@ import axios from "axios";
 // Switch API address based on environment
 const API_BASE_URL =
     import.meta.env.MODE === "production"
-        ? "https://care-scheduling-app-e8951cd9f9c6.herokuapp.com"
-        : "http://localhost:8080";
+        ? "https://care-scheduling-app-e8951cd9f9c6.herokuapp.com/api"
+        : "http://localhost:8081/api";
 
 const api = axios.create({
     baseURL: API_BASE_URL,
